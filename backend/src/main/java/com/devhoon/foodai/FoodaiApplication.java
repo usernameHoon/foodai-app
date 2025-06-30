634a1b708a73c8c4a43023d24a856ee7c87c014c
usernameHoon <gns326@naver.com>
2025-06-30 11:57:38 +0900

    Spring Boot 메인 클래스 FoodaiApplication.java 생성

---
+++ backend/src/main/java/com/devhoon/foodai/FoodaiApplication.java
@@ -0,0 +1,13 @@
+package com.devhoon.foodai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodaiApplication.class, args);
+	}
+
+}
