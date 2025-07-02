cd4e196422689230b10f0fa4b7747978d2c2befd
usernameHoon <gns326@naver.com>
2025-07-02 17:08:59 +0900

    로그인 요청 DTO 클래스 추가

---
+++ backend/src/main/java/com/devhoon/foodai/dto/LoginRequestDTO.java
@@ -0,0 +1,24 @@
+package com.devhoon.foodai.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+
+  @NotBlank
+  @Email
+  private String email; // 이메일
+
+  @NotBlank
+  private String password; // 비밀번호
+
+  @NotBlank
+  private String role; // 권한
+}
