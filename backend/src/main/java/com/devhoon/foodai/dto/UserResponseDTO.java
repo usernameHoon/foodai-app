8223756d39a8dadc03276151bbbe11e632c275fd
usernameHoon <gns326@naver.com>
2025-07-02 17:09:08 +0900

    사용자 응답 데이터 DTO 클래스 추가

---
+++ backend/src/main/java/com/devhoon/foodai/dto/UserResponseDTO.java
@@ -0,0 +1,12 @@
+package com.devhoon.foodai.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+
+  private String email;
+  private String name;
+}
