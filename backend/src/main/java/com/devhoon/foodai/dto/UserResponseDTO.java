91a700acc050a691be3b98a975f052e9d2690f74
usernameHoon <gns326@naver.com>
2025-07-04 13:57:02 +0900

    사용자 응답 정보를 위한 DTO 클래스 추가

--- backend/src/main/java/com/devhoon/foodai/dto/UserResponseDTO.java
+++ backend/src/main/java/com/devhoon/foodai/dto/UserResponseDTO.java
@@ -1,5 +1,7 @@
 package com.devhoon.foodai.dto;
 
+import com.devhoon.foodai.entity.User;
+
 import lombok.*;
 
 @Data
@@ -7,6 +9,18 @@ import lombok.*;
 @AllArgsConstructor
 public class UserResponseDTO {
 
+  private Long id;
   private String email;
   private String name;
+  private String role;
+  private boolean isDeleted;
+
+  // User 엔티티 기반 생성자
+  public UserResponseDTO(User user) {
+    this.id = user.getId();
+    this.email = user.getEmail();
+    this.name = user.getName();
+    this.role = user.getRole().name();
+    this.isDeleted = user.isDeleted();
+  }
 }
