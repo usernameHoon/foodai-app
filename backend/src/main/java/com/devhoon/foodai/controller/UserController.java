185d7830deaec67d4b45ebdd11bc800ffd59cb52
usernameHoon <gns326@naver.com>
2025-07-04 13:57:47 +0900

    사용자 관련 API 컨트롤러 구현

--- backend/src/main/java/com/devhoon/foodai/controller/UserController.java
+++ backend/src/main/java/com/devhoon/foodai/controller/UserController.java
@@ -1,20 +1,16 @@
 package com.devhoon.foodai.controller;
 
 import com.devhoon.foodai.dto.LoginRequestDTO;
+import com.devhoon.foodai.dto.LoginResponseDTO;
 import com.devhoon.foodai.dto.UserRequestDTO;
-
-import com.devhoon.foodai.entity.User;
-import com.devhoon.foodai.service.JwtService;
+import com.devhoon.foodai.security.CustomUserDetails;
 import com.devhoon.foodai.service.UserService;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -22,25 +18,11 @@ import java.util.Map;
 public class UserController {
 
   private final UserService userService;
-  private final JwtService jwtService;
 
   // 로그인
   @PostMapping("/login")
-  public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDTO request) {
-    User user = userService.authenticateUser(request.getEmail(), request.getPassword());
-
-    if (user == null) {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", "이메일 또는 비밀번호가 일치하지 않습니다."));
-    }
-
-    String token = jwtService.generateToken(user);
-
-    Map<String, String> response = new HashMap<>();
-    response.put("token", token);
-    response.put("email", user.getEmail());
-    response.put("name", user.getName());
-    response.put("role", user.getRole().name()); // 예: "ADMIN" 또는 "USER"
-
+  public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginDto) {
+    LoginResponseDTO response = userService.login(loginDto.getEmail(), loginDto.getPassword());
     return ResponseEntity.ok(response);
   }
 
@@ -49,5 +31,13 @@ public class UserController {
   public ResponseEntity<?> signup(@RequestBody UserRequestDTO requestDTO) {
     userService.registerUser(requestDTO);
     return ResponseEntity.ok("회원가입 성공");
+  }
+
+  // 회원탈퇴
+  @DeleteMapping("/withdraw")
+  public ResponseEntity<String> withdraw(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    Long userId = userDetails.getUser().getId(); // 또는 getId()
+    userService.withdrawUser(userId);
+    return ResponseEntity.ok("회원 탈퇴 완료");
   }
 }
